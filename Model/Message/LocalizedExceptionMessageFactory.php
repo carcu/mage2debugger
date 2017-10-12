11657abcb1c49164a32c597b4b74bc4240ccb312
carcu <cristian@itwebexperts.com>
2017-10-13 02:23:22 +0300

    compat with 1.9

--- Model/Message/LocalizedExceptionMessageFactory.php
+++ Model/Message/LocalizedExceptionMessageFactory.php
@@ -7,53 +7,99 @@
 namespace SalesIgniter\Debugger\Model\Message;
 
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Message\ExceptionMessageFactoryInterface;
 use Magento\Framework\Message\MessageInterface;
 use Magento\Framework\Message\Factory;
 use Magento\Framework\Exception\RuntimeException;
-
-class LocalizedExceptionMessageFactory implements ExceptionMessageFactoryInterface
-{
-    /**
-     * @var \Magento\Framework\Message\Factory
-     */
-    private $messageFactory;
-
-    /**
-     * @var \Magento\Framework\UrlInterface
-     */
-    private $urlBuilder;
-
-    /**
-     * @param Factory                         $messageFactory
-     * @param \Magento\Framework\UrlInterface $urlBuilder
-     */
-    public function __construct(Factory $messageFactory, \Magento\Framework\UrlInterface $urlBuilder)
+if (class_exists(\Magento\Framework\Message\ExceptionMessageFactoryInterface::class)) {
+    class LocalizedExceptionMessageFactory implements \Magento\Framework\Message\ExceptionMessageFactoryInterface
     {
-        $this->messageFactory = $messageFactory;
-        $this->urlBuilder = $urlBuilder;
-    }
+        /**
+         * @var \Magento\Framework\Message\Factory
+         */
+        private $messageFactory;
 
-    /**
-     * {@inheritdoc}
-     *
-     * @throws \InvalidArgumentException
-     */
-    public function createMessage(\Exception $exception, $type = MessageInterface::TYPE_ERROR)
-    {
-        if ($exception instanceof LocalizedException) {
-            try {
-                if (class_exists('\SalesIgniter\Debugger\Helper\Data')) {
-                    $myDebugger = \Magento\Framework\App\ObjectManager::getInstance()->get('\SalesIgniter\Debugger\Helper\Data');
-                    $myDebugger->addDataWithTrace($exception);
-                }
-            } catch (\Exception $e) {
-            }
-            return $this->messageFactory->create($type)
-                ->setText($exception->getMessage());
+        /**
+         * @var \Magento\Framework\UrlInterface
+         */
+        private $urlBuilder;
+
+        /**
+         * @param Factory                         $messageFactory
+         * @param \Magento\Framework\UrlInterface $urlBuilder
+         */
+        public function __construct(Factory $messageFactory, \Magento\Framework\UrlInterface $urlBuilder)
+        {
+            $this->messageFactory = $messageFactory;
+            $this->urlBuilder = $urlBuilder;
         }
-        throw new RuntimeException(
-            __('Exception instance doesn\'t match %1 type', UrlAlreadyExistsException::class)
-        );
+
+        /**
+         * {@inheritdoc}
+         *
+         * @throws \InvalidArgumentException
+         */
+        public function createMessage(\Exception $exception, $type = MessageInterface::TYPE_ERROR)
+        {
+            if ($exception instanceof LocalizedException) {
+                try {
+                    if (class_exists('\SalesIgniter\Debugger\Helper\Data')) {
+                        $myDebugger = \Magento\Framework\App\ObjectManager::getInstance()->get('\SalesIgniter\Debugger\Helper\Data');
+                        $myDebugger->addDataWithTrace($exception);
+                    }
+                } catch (\Exception $e) {
+                }
+                return $this->messageFactory->create($type)
+                    ->setText($exception->getMessage());
+            }
+            throw new RuntimeException(
+                __('Exception instance doesn\'t match %1 type', UrlAlreadyExistsException::class)
+            );
+        }
+    }
+} else{
+    class LocalizedExceptionMessageFactory
+    {
+        /**
+         * @var \Magento\Framework\Message\Factory
+         */
+        private $messageFactory;
+
+        /**
+         * @var \Magento\Framework\UrlInterface
+         */
+        private $urlBuilder;
+
+        /**
+         * @param Factory                         $messageFactory
+         * @param \Magento\Framework\UrlInterface $urlBuilder
+         */
+        public function __construct(Factory $messageFactory, \Magento\Framework\UrlInterface $urlBuilder)
+        {
+            $this->messageFactory = $messageFactory;
+            $this->urlBuilder = $urlBuilder;
+        }
+
+        /**
+         * {@inheritdoc}
+         *
+         * @throws \InvalidArgumentException
+         */
+        public function createMessage(\Exception $exception, $type = MessageInterface::TYPE_ERROR)
+        {
+            if ($exception instanceof LocalizedException) {
+                try {
+                    if (class_exists('\SalesIgniter\Debugger\Helper\Data')) {
+                        $myDebugger = \Magento\Framework\App\ObjectManager::getInstance()->get('\SalesIgniter\Debugger\Helper\Data');
+                        $myDebugger->addDataWithTrace($exception);
+                    }
+                } catch (\Exception $e) {
+                }
+                return $this->messageFactory->create($type)
+                    ->setText($exception->getMessage());
+            }
+            throw new RuntimeException(
+                __('Exception instance doesn\'t match %1 type', UrlAlreadyExistsException::class)
+            );
+        }
     }
 }
